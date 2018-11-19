5cdcdc005319d69ee006a4888440d6fed4d12d43
kevahere <kevahere@gmail.com>
2018-11-19 10:57:52 +0300

    addded ratings views

--- prizes/views.py
+++ prizes/views.py
@@ -1,5 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile, Project
+from .forms import UpdateProfile, PostImageForm, ratingForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
 def index(request):
-    return render(request,'index.html')
+    projects = Project.get_all()
+    return render(request, 'index.html', {'pics': pics})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    user = request.user
+    profiles = Profile.get_user(user.id)
+    projects = Project.get_by_user(user.id)
+    if profiles:
+        profile = profiles[len(profiles)-1]
+    else:
+        profile = profiles
+
+    if request.method == 'POST':
+        profile_form = UpdateProfile(request.POST, request.FILES)
+        upload_form = PostImageForm(request.POST, request.FILES)
+        rating_form = ratingForm(request.POST)
+        if profile_form.is_valid():
+            if Profile.get_user(user.id):
+                deleter = Profile.get_user(user.id)
+                deleter.delete()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save_profile()
+            return HttpResponseRedirect('/profile')
+        if upload_form.is_valid():
+            image = upload_form.save(commit=False)
+            image.user = user
+            image.save_image()
+    else:
+        profile_form = UpdateProfile()
+        upload_form = PostImageForm()
+        rating_form = ratingForm
+    return render(request, 'registration/profile.html', {'user': user, 'profile': profile, 'pics': pics, 'profile_form': profile_form,
+                                            'upload_form': upload_form, 'coment_form': rating_form})
+
+
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        search_projects = Project.search_project(search_term)
+        search_prof = Profile.search_profiles(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message': message, 'pics': search_pics, 'profiles': search_prof})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {'message': message})
+
+@login_required(login_url='/accounts/login/')
+def single_project(request, project):
+    user = request.user
+    project = Project.get_img_by_id(project)
+    if request.method == 'POST':
+        rating_form = ratingForm(request.POST)
+        if rating_form.is_valid():
+            rating = rating_form.save(commit=False)
+            rating.user = user
+            rating.image = picture
+            rating.save()
+            return redirect('picture', picture.id)
+    else:
+        rating_form = ratingForm()
+
+    return render(request, 'project.html', {'rating_form': rating_form, 'project': project, 'user': user, 'ratings': ratings})
